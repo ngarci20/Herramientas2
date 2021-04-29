856dfe1348b2c8fc558901b3c6a07f7295c96478
ngarci20 <63400832+ngarci20@users.noreply.github.com>
2021-04-29 17:27:00 +0200

    Add files via upload

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,8 @@
+import * as bootstrap from 'bootstrap';
+import '@fortawesome/fontawesome-free/js/all.min';
+
+
++( function() {
+  const university = "UOC";
+  console.log(`Hello, ${university}!`);
+} )();
